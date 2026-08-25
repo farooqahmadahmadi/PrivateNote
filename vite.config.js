5ba2aacaf2f82f4a9e22edae3a6db8565cff00b2
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-08-25 07:44:23 +0430

    Upadeted form inastall

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: "./",
+
+  plugins: [react(), tailwindcss()],
 
   server: {
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     port: 5173,
     strictPort: true,
   },
-})
+});
